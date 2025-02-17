fef01180015978ecc9252697aa725e826fbfa0f4
m4rt1nn <kodapa@gmail.com>
2025-02-17 16:33:23 +0100

    Added swagger

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-# app.py
 from flask import Flask, jsonify, request
 import requests, certifi
+from flask_restx import Api, Resource, fields
 
 app = Flask(__name__)
 
-# API Route: /api/random_joke (hämtar en rolig gåta från ett externt API)
-@app.route('/api/random_joke', methods=['GET'])
-def random_joke():
-    # Anropar ett externt API för att hämta en slumpmässig skämt
-    response = requests.get('https://v2.jokeapi.dev/joke/Programming?type=twopart', verify=certifi.where())
-    joke = response.json()
+api = Api(app, doc='/swagger', prefix='/api')
+ns = api.namespace('jokes', description='Fetch funny programming jokes')
+api.add_namespace(ns)
+joke_model = api.model('Joke', {
+    'setup': fields.String(description='Question or setup of joke', required=True),
+    'punchline': fields.String(description='Punchline or end of joke', required=True)
+})
 
-    return jsonify(setup=joke['setup'], punchline=joke['delivery'])
+
+# Definiera en resurs för att hämta ett skämt
+@ns.route('/random')
+class Joke(Resource):
+    @api.marshal_with(joke_model)
+    def get(self):
+        """
+        Return a random joke.
+        """
+        response = requests.get('https://v2.jokeapi.dev/joke/Programming?type=twopart', verify=certifi.where())
+        joke = response.json()
+        return {
+            "setup": joke['setup'],
+            "punchline": joke['delivery']
+        }
 
 if __name__ == '__main__':
     app.run(debug=True)
